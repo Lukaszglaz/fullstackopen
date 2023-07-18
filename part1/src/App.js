98a6dff6dc50c3658e4e446b8005686f2388b775
Lukaszglaz <glazzlukasz@gmail.com>
2023-07-18 21:02:16 +0200

    edit main contents

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,19 +1,25 @@
-import React from "react";
-
 const App = () => {
-  const friends = [
-    { name: "Peter", age: 4 },
-    { name: "Maya", age: 10 },
-  ];
+  const course = "Half Stack application development";
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
 
   return (
     <div>
+      <h1>{course}</h1>
       <p>
-        {friends[0].name} {friends[0].age}
+        {part1} {exercises1}
       </p>
       <p>
-        {friends[1].name} {friends[1].age}
+        {part2} {exercises2}
       </p>
+      <p>
+        {part3} {exercises3}
+      </p>
+      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
     </div>
   );
 };
