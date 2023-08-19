83bd5dc226eeb0b0ec91d9c1836772f7840260e6
Lukaszglaz <glazzlukasz@gmail.com>
2023-08-19 12:23:45 +0200

    1.2 Courseinfo

---
+++ part1/src/components/Content.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Part = ({ part, exercises }) => {
+  return (
+    <p>
+      {part} {exercises}
+    </p>
+  );
+};
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      {parts.map((part) => (
+        <Part key={part.name} part={part.name} exercises={part.exercises} />
+      ))}
+    </div>
+  );
+};
+
+export default Content;
